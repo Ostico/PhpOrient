4452a3e8e0758d054b927dd1509e16fd275ea21d
Domenico Lupinetti <ostico@gmail.com>
2014-11-30 22:52:37 +0100

    Fixed Travis build scripts

--- ci/initialize-ci.sh
+++ ci/initialize-ci.sh
@@ -11,26 +11,26 @@ echo "=== Initializing CI environment ==="
 
 cd "$PARENT_DIR"
 
-. "$PARENT_DIR/ci/odb-shared.sh"
+. "$PARENT_DIR/ci/_bash_utils.sh"
 
 if [ ! -d "$ODB_DIR" ]; then
   # Download and extract OrientDB server
   echo "--- Downloading OrientDB v${ODB_VERSION} ---"
-  odb_download_via_mvn $ODB_VERSION $CI_DIR
+  build ${ODB_VERSION} ${CI_DIR}
 
   # Ensure that launcher script is executable and copy configurations file
   echo "--- Setting up OrientDB ---"
-  chmod +x $ODB_LAUNCHER
+  chmod +x ${ODB_LAUNCHER}
   chmod -R +rw "${ODB_DIR}/config/"
-  cp $PARENT_DIR/ci/orientdb-server-config.xml "${ODB_DIR}/config/"
-  cp $PARENT_DIR/ci/orientdb-server-log.properties "${ODB_DIR}/config/"
+  cp ${PARENT_DIR}/ci/orientdb-server-config.xml "${ODB_DIR}/config/"
+  cp ${PARENT_DIR}/ci/orientdb-server-log.properties "${ODB_DIR}/config/"
 else
   echo "!!! Found OrientDB v${ODB_VERSION} in ${ODB_DIR} !!!"
 fi
 
 # Start OrientDB in background.
 echo "--- Starting an instance of OrientDB ---"
-sh -c $ODB_LAUNCHER </dev/null &>/dev/null &
+sh -c ${ODB_LAUNCHER} </dev/null &>/dev/null &
 
 # Wait a bit for OrientDB to finish the initialization phase.
 sleep 5
