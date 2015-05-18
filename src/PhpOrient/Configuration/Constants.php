f9eea6af2b648accb54a7bdb28807d0367e62267
Domenico Lupinetti <Ostico@users.noreply.github.com>
2015-05-19 00:17:02 +0200

    Update Constants.php
    Updated version an supported protocol

--- src/PhpOrient/Configuration/Constants.php
+++ src/PhpOrient/Configuration/Constants.php
@@ -14,7 +14,7 @@ class Constants {
      * @const string the client version.
      *
      */
-    const VERSION = "v1.1.7";
+    const VERSION = "v1.1.8";
     const ID = "1";
     const NAME = "PhpOrient - PHP binary client for OrientDB";
     const SUPPORTED_PROTOCOL = 29;
