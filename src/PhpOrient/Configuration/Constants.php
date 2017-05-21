78155eed96ff294d759a10b595d343a35799c87d
Domenico Lupinetti <ostico@gmail.com>
2017-05-21 21:09:05 +0200

    Removed Log

--- src/PhpOrient/Configuration/Constants.php
+++ src/PhpOrient/Configuration/Constants.php
@@ -17,7 +17,7 @@ class Constants {
     const VERSION = "v1.2.5";
     const ID = "1";
     const NAME = "PhpOrient - Php driver based on the binary protocol of OrientDB.";
-    const SUPPORTED_PROTOCOL = 37;
+    const SUPPORTED_PROTOCOL = 36;
     public static $LOGGING = false;
     public static $LOG_FILE_PATH = false;
 
